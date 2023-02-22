f0df08d9c8f16549c743da38ccba9c9367cdcf82
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-02-22 12:56:07 +0530

    change gmi import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Gaadi_Malik_Issue from "./components/Gaadi-Malik-Issue";
+import Gaadi_Malik_Issue from "./components/GMI/Gaadi-Malik-Issue";
 
 export default function App() {
   return (
     <>
-      <p>Hello</p>
       <Gaadi_Malik_Issue />
     </>
   );
