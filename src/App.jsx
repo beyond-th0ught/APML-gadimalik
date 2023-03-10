c11e5d5d3d5bb4d9dbc76bea0c865eb2c0071094
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-03-10 13:07:35 +0530

    render New_Tyre_Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import New_Tyre_Issue from "./components/ControlRoom/NTI/New-Tyre-Issue";
 import Gaadi_Malik_Issue from "./components/GMI/Gaadi-Malik-Issue";
 
 export default function App() {
   return (
     <>
-      <Gaadi_Malik_Issue />
+      {/* <Gaadi_Malik_Issue /> */}
+      <New_Tyre_Issue />
     </>
   );
 }
