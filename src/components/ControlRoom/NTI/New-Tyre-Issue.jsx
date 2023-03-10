6a561b69a487fba09d939a1e64fa3b986259489e
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-03-10 13:06:53 +0530

    feat: show new tyre issue data

--- src/components/ControlRoom/NTI/New-Tyre-Issue.jsx
+++ src/components/ControlRoom/NTI/New-Tyre-Issue.jsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 // IMPORT FUNCTIONS
-import { fetchLoadData, fetchEmptyData } from "./ntiFunctions";
+import { fetchData } from "./ntiFunction";
 
 export default function New_Tyre_Issue() {
   const [loadedContent, setLoadedContent] = useState([]);
-  const [emptyContent, setEmptyContent] = useState([]);
 
-  const [lC, setLC] = useState(null);
-  const [eC, setEC] = useState(null);
+  // const [lC, setLC] = useState(null);
+  // const [eC, setEC] = useState(null);
 
   useEffect(() => {
     (async () => {
-      setLoadedContent(await fetchLoadData());
-      setLC(loadedContent.length);
-      setEmptyContent(await fetchEmptyData());
-      setEC(emptyContent.length);
+      setLoadedContent(await fetchData());
     })();
   }, []);
 
@@ -23,7 +19,7 @@ export default function New_Tyre_Issue() {
     <>
       <div className="Section1">
         <div>
-          <p>{lC}</p>
+          {/* <p>{lC}</p> */}
           <div>
             <p className="caseno" type="text" disabled></p>
             Gaadi malik Issue (Loaded)
@@ -35,7 +31,8 @@ export default function New_Tyre_Issue() {
                 <th>V No.</th>
                 <th></th>
                 <th>GM</th>
-                <th>Problem</th>
+                <th>Work Place</th>
+                <th>Status</th>
                 <th>Description</th>
                 <th>Time Up </th>
               </tr>
@@ -68,67 +65,10 @@ export default function New_Tyre_Issue() {
                       </span>
                     </td>
                     <td>{e.gm}</td>
-                    <td>{e.prob}</td>
-                    <td>{e.desc}</td>
-                    <td value="hello"></td>
-                    <td>{e.temp}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-
-        <div>
-          <p>{eC}</p>
-          <div>
-            <p className="caseno" type="text" disabled></p>
-            Gaadi malik Issue (Empty)
-          </div>
-
-          <table border={1}>
-            <thead>
-              <tr>
-                <th>V No.</th>
-                <th></th>
-                <th>GM</th>
-                <th>Problem</th>
-                <th>Description</th>
-                <th>Time Up </th>
-              </tr>
-            </thead>
-            <tbody>
-              {emptyContent.map((e) => {
-                return (
-                  <tr>
-                    <td>{e.vecNo || "NA"}</td>
-                    <td>
-                      <span>
-                        {e.len}
-                        <img
-                          // style="background-color: black"
-                          src={e.compLogo}
-                          width="40px"
-                          height="25px"
-                        />
-                        |{e.details.year}|{e.details.cover}|{e.details.type}|
-                      </span>
-                      <span>{e.run}</span> | <span>{e.rDetails.speed}</span> |{" "}
-                      <span>{e.rDetails.halt}</span> |{" "}
-                      <span>
-                        <img
-                          // style="background-color: black"
-                          src={e.shipLogo}
-                          width="55px"
-                          height="25px"
-                        />
-                      </span>
-                    </td>
-                    <td>{e.gm}</td>
-                    <td>{e.prob}</td>
+                    <td>{e.workPlace}</td>
+                    <td>{e.currStatus}</td>
                     <td>{e.desc}</td>
                     <td>{e.time}</td>
-                    <td>{e.temp}</td>
                   </tr>
                 );
               })}
