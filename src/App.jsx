765a77a5acd1e277cd78ae4d697e0f06c45fa87f
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-03-15 12:41:00 +0530

    chore: render GDI

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Gaadi_Malik_Issue from "./components/GMI/Gaadi-Malik-Issue";
 export default function App() {
   return (
     <>
-      {/* <Gaadi_Malik_Issue /> */}
-      <New_Tyre_Issue />
+      <Gaadi_Malik_Issue />
+      {/* <New_Tyre_Issue /> */}
     </>
   );
 }
