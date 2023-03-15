d193535c7dee0502208b78e6f258f032202634e5
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-03-15 15:57:24 +0530

    chore: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import New_Tyre_Issue from "./components/ControlRoom/NTI/New-Tyre-Issue";
+
 import Gaadi_Malik_Issue from "./components/GMI/Gaadi-Malik-Issue";
+import New_Tyre_Issue from "./components/ControlRoom/NTI/New-Tyre-Issue";
 
 export default function App() {
   return (
